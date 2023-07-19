1c3bd7e20af83f8d0f05c4cba94b9dc0f3bd3103
whyest <whyest@gmail.com>
2023-07-19 16:37:28 +0300

    first commit

---
+++ src/Modal.jsx
@@ -0,0 +1,17 @@
+import { useGlobalContext } from './Context'
+import { FaTimes } from 'react-icons/fa'
+
+const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext()
+  return (
+    <div className={isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}>
+      <div className='modal-container'>
+        <h3>modal content is here</h3>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
+}
+export default Modal
